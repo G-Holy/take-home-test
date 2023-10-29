00b24390ec0447ec8de1b43e43165f1a6f254ea4
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-10-29 17:20:55 +0100

    test(pharmacy): a normal drug should decrease benefit value twice as fast when expired

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -4,8 +4,14 @@ describe("Pharmacy", () => {
   describe("Given a normal drug at the end of the day", () => {
     it("Should lowers expiresIn and benefit values by 1", () => {
       expect(
-        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
-      ).toEqual([new Drug("test", 1, 2)]);
+        new Pharmacy([new Drug("normal drug", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("normal drug", 1, 2)]);
+    });
+
+    it("Should decrease benefit value twice as fast when expired", () => {
+      expect(
+        new Pharmacy([new Drug("normal drug", 0, 4)]).updateBenefitValue()
+      ).toEqual([new Drug("normal drug", -1, 2)]);
     });
   });
 });
