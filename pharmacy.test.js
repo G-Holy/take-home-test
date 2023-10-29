8ff0ec14ed64d72987c08990e1051f271c1b37de
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-10-29 17:12:46 +0100

    test(pharmacy): a normal drug should lowers expiresIn and benefit values by 1 at the end of the day

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,11 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("Given a normal drug at the end of the day", () => {
+    it("Should lowers expiresIn and benefit values by 1", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
   });
 });
