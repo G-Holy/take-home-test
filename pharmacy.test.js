75556a5b09f392c688a6cde17e8f90d2accb47b8
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-10-29 17:30:26 +0100

    test(pharmacy): herbal tea should increase in benefit twice as fast when expired

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -14,11 +14,18 @@ describe("Pharmacy", () => {
       ).toEqual([new Drug("normal drug", -1, 2)]);
     });
   });
+
   describe("Given herbal tea at the end of the day", () => {
     it("should increase in benefit by 1", () => {
       expect(
         new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()
       ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+    });
+
+    it("should increase in benefit twice as fast when expired", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 0, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", -1, 5)]);
     });
   });
 });
