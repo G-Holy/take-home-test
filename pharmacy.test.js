cb6e8da644b615a73029ba4139accc4d158d5590
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-10-29 17:38:30 +0100

    test(pharmacy): any kind of drugs should never have benefit value above 50

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,6 +1,17 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
+  describe("Given any kind of drugs", () => {
+    it("should never have benefit value above 50", () => {
+      expect(
+        new Pharmacy([
+          new Drug("Herbal Tea", 2, 50),
+          new Drug("Fervex", 2, 50),
+        ]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", 1, 50), new Drug("Fervex", 1, 50)]);
+    });
+  });
+
   describe("Given a normal drug at the end of the day", () => {
     it("Should lowers expiresIn and benefit values by 1", () => {
       expect(
