b1b1d13459b8fd6a27731879ce1c15ddb7da2069
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-10-30 21:55:20 +0100

    chore(pharmacy): fix lint errors

--- src/value-objects/expires-in/expires-in.js
+++ src/value-objects/expires-in/expires-in.js
@@ -1,14 +1,13 @@
 export class ExpiresIn {
-    constructor(days) {
-      this.days = days
-     }
-    
-    updateToNextDay() {
-      return new ExpiresIn(this.days - 1)
-    }
-  
-    isExpired() {
-      return this.days < 0
-    }
+  constructor(days) {
+    this.days = days;
+  }
+
+  updateToNextDay() {
+    return new ExpiresIn(this.days - 1);
+  }
+
+  isExpired() {
+    return this.days < 0;
+  }
 }
-  
